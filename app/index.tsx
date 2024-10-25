8150ddb83d85c6e9b1069eb4579aa04c1a899b29
ritikdevelop <hrithikrana216@gmail.com>
2024-10-25 15:12:06 +0530

    deleted unused file and added required assets

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
